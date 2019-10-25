b33297f97f13c647b8c892d204143f416d6cf441
mmebsout <magalimebsout+dev@gmail.com>
2019-10-25 16:23:26 +0200

    Working server (no cube yet)

--- docker/run.sh
+++ docker/run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-docker run --name datacube -d -p 8000:4200 datacube
+docker run --name datacube -d -p 8000:4200 -p 8081:8081 datacube
 echo "Please launch : http://localhost:8000/demo/DataCube"
