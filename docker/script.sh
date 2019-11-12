831cdc497810069ceb9ef1921d6072094760b8e2
mmebsout <magalimebsout+dev@gmail.com>
2019-11-12 17:43:34 +0100

    Added a bind-mount to access datas

--- docker/script.sh
+++ docker/script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 npm --prefix /opt/DataCube start & 
-java -jar /opt/DataCubeServer/target/cubeExplorer-1.0.0-SNAPSHOT.jar
+java -jar /opt/DataCubeServer/target/cubeExplorer-1.0.0-SNAPSHOT.jar 
