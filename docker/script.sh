26df052b9e79a8423544aef0ff9105c8e7ec6656
mmebsout <magalimebsout+dev@gmail.com>
2019-11-14 11:51:04 +0100

    Edit files in place instead of using copies + default cube in the container as well

--- docker/script.sh
+++ docker/script.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 npm --prefix /opt/DataCube start & 
-java -jar /opt/DataCubeServer/target/cubeExplorer-1.0.0-SNAPSHOT.jar 
+
+cd /opt/DataCubeServer/target/
+java -jar "$(echo cubeExplorer-*.jar )"
